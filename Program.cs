3516812c160ec049c2b8c1c5701af306ed3d2d73
joao vitor <githubconta2021@gmail.com>
2023-11-28 12:27:35 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+using Program.Entities;
+using Program.Entities.Exception;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main()
+        {
+          try
+          {
+             // entrada de dados 
+
+           Console.WriteLine("Enter account data");
+
+           Console.Write("Number: ");
+           int number = int.Parse(Console.ReadLine());
+
+           Console.Write("Holder: ");
+           string holder = Console.ReadLine();
+
+           Console.Write("Initial balance: ");
+           double inicialBalance = double.Parse(Console.ReadLine());
+
+           Console.Write("Withdraw limit: ");
+           double withdrawLimit = double.Parse(Console.ReadLine());
+           
+           // processando dados  
+
+           Account account = new Account(number, holder, inicialBalance, withdrawLimit); 
+
+             // funcionalidade de sacar o dinheiro
+
+                Console.WriteLine(); 
+
+                Console.Write("Enter amount for withdraw: ");
+                double amountWithdraw = double.Parse(Console.ReadLine());
+                account.Withdrawlimit(amountWithdraw);
+
+            // saida de dados
+            Console.WriteLine(account);
+          }
+          catch (DomainException e)
+          {
+            Console.WriteLine($" Withdraw error: {e.Message}");
+          }
+          catch (FormatException e)
+          {
+            Console.WriteLine($"Format error: {e.Message}");
+          }
+          catch (Exception e)
+          {
+            Console.WriteLine($"Unexpected error: {e.Message}");
+          }
+        }
+   }
+}
+
